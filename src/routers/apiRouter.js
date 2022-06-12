3a3cdf9cb7f532cc92332925d54875088b82e75e
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-12 16:25:09 +0900

    Update apiRouter.js

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerView, createComment } from "../controllers/videoController";
+import { registerView, createComment, deleteComment, likeComment} from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
+apiRouter.post("/videos/:id([0-9a-f]{24})/delete", deleteComment);
+apiRouter.post("/videos/:id([0-9a-f]{24})/like", likeComment);
 
 export default apiRouter;
