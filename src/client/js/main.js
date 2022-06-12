0944344f71e8e617e99f87c70ca2b37dd2e3b71a
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-12 16:25:26 +0900

    Update main.js

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,25 +1,16 @@
 import "regenerator-runtime";
 import "../scss/styles.scss";
 
-// const videoMixin = document.querySelector(".video-mixin");
-// const videoMixinData = document.querySelector(".video-mixin__data");
-// let controlsTimeout = null;
+const videoMixinAll = document.querySelectorAll(".video-mixin");
+let controlsTimeout = null;
+
+const handleMouseMove = (event) => {
+    controlsTimeout = setTimeout(()=>{
+        event.path[0].classList.add("disappear")
+    }, 1000);
+}
 
 
-// const hideControls = () => {
-//     videoMixinData.classList.add("disappear")
-// }
-
-// const handleMouseMove = () => {
-//     const { id } = videoMixin.dataset;
-//     console.log(id)
-//     controlsTimeout = setTimeout(hideControls, 1000);
-// }
-
-// const handleMouseLeave = () => {
-//     console.log('seo')
-// }
-
-  
-// videoMixin.addEventListener("mousemove", handleMouseMove);
-// videoMixin.addEventListener("mouseleave", handleMouseLeave);
+videoMixinAll.forEach((videoMixin) => {
+    videoMixin.addEventListener("mousemove", handleMouseMove)
+})
