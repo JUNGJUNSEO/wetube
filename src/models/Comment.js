3b4d3809cb8ce3a97bfc39bb800f34e66cbeba82
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-12 16:25:18 +0900

    Update Comment.js

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -5,6 +5,9 @@ const commentSchema = new mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   video: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Video" },
   createdAt: { type: Date, required: true, default: Date.now },
+  likes: { type: Number, default: 0, required: true },
+  avatarUrl: String,
+  name: { type: String },
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
