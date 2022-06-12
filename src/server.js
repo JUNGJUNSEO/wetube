0160f67f08d42e3ab3f151f43c648419ddf0ff97
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-12 23:55:09 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
-import flash from "express-flash";
+
 
 const app = express();
 
@@ -15,18 +16,18 @@ app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
-    session({
-      secret: process.env.COOKIE_SECRET,
-      resave: true,
-      saveUninitialized: true,
-      store: MongoStore.create({mongoUrl: process.env.DB_URL})
-    })
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
 );
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
-app.use("/", rootRouter); 
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/api", apiRouter);
