0c1349a8e4dd63a20702762549b6fb02b163c61b
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-08-22 20:05:23 +0900

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -9,13 +9,11 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String },
   location: String,
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", function () {
   if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 5);
+    this.password = bcrypt.hash(this.password, 5);
   }
 });
 
